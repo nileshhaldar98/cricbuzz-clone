7d6ef8910753f04cc923b44ff11cf1555b70105f
nileshhaldar98 <nileshhaldar98@gmail.com>
2022-07-24 11:50:47 +0530

    all updated

--- js/login.js
+++ js/login.js
@@ -4,18 +4,29 @@ function login() {
   var value = JSON.parse(localStorage.getItem("userData"));
   let userEamil = document.querySelector("#email").value;
   let userPassword = document.querySelector("#password").value;
-
+let valu=true;
   if (value === null) {
     alert("User Not registerd ");
     return;
   }
+  let arr=JSON.parse(localStorage.getItem("userInfo"))||[];
   value.forEach(function (elem) {
     if (userEamil === elem.userEmail && userPassword === elem.userPassword) {
-      location.href = "index.html";
+      arr.push(elem);
+      valu=false;
+      localStorage.setItem("userInfo",JSON.stringify(arr));
+      location.href = "loginIndex.html";
+      return;
     } else {
-      alert("you enterd wrong email and password");
+      valu=true;
     }
   });
+
+
+  if(valu){
+    alert("you enterd wrong email and password");
+  }
+
 }
 document.querySelector(".home").addEventListener("click", home);
 function home() {
---
+++ js/profile.js
@@ -0,0 +1,2 @@
+document.getElementById("username").value="nileshhaldar98";
+document.getElementById("emailA").value="nileshhaldar98@gmail.com";
